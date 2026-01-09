e9d195bfeb5a3985974590159ae2522f5ce39340
jose97-ai <148401379+jose97-ai@users.noreply.github.com>
2026-01-09 03:12:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -364,7 +364,7 @@ def bloque_1_censo():
         st.table(df_reciente)
 
     conn.close()
-    def inicializar_tablas_sistema():
+def inicializar_tablas_sistema():
     conn = sqlite3.connect('aps_oran_final.db')
     cursor = conn.cursor()
     # Tabla de Personas
@@ -1469,6 +1469,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
